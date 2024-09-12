ac67e486867a665e5e698a9381ff25ff61751663
Milan-Kotadiya <mvkotadiya50@gmail.com>
2024-09-12 23:08:02 +0530

    updated ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,20 @@
+// module.exports = {
+//   apps: [
+//     {
+//       name: "Agroveda-Backend",
+//       script:
+//         "/home/milan/runners/backend/actions-runner/_work/Backend/Backend/dist", // /home/milan/runners/backend/actions-runner/_work/Backend/Backend
+//       watch: false,
+//     },
+//   ],
+// };
 module.exports = {
   apps: [
     {
-      name: "Agroveda-Backend",
-      script:
-        "/home/milan/runners/backend/actions-runner/_work/Backend/Backend/dist", // /home/milan/runners/backend/actions-runner/_work/Backend/Backend
-      watch: false,
+      name: "Agroveda",
+      script: "./dist/index.js",
+      instances: "max",
+      exec_mode: "cluster",
     },
   ],
 };
