4a354ba7d4860b401e2aed3b2af3975657c4a2ae
Milan-Kotadiya <mvkotadiya50@gmail.com>
2024-09-12 23:25:59 +0530

    Updated New

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,12 +3,18 @@ module.exports = {
     {
       name: "Agroveda",
       script: "./dist/index.js", // Ensure this points to the correct file
-      instances: "max", // Use 'max' to utilize all CPU cores or set a specific number
-      exec_mode: "cluster",
-      watch: false, // Set to true if you want to watch for file changes
+      instances: 1, // Set to a specific number or 'max' for all CPU cores
+      exec_mode: "cluster", // Use 'fork' if you don't need clustering
+      watch: false, // Set to true if you want PM2 to watch for file changes
       env: {
         NODE_ENV: "production",
       },
+      // Optional: Customize logs and other settings if needed
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      error_file: "./logs/err.log",
+      out_file: "./logs/out.log",
+      merge_logs: true,
+      autorestart: true,
     },
   ],
 };
