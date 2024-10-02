1117ec0e446bd1f44b3f2945ab89ded3b3ff8647
Milan-Kotadiya <mvkotadiya50@gmail.com>
2024-10-02 22:46:56 +0530

    Cluster Added

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,8 @@ import { initializeServer } from "./connections/http.connection.js";
 import dotenv from "dotenv";
 import { initializeDatabase } from "./connections/database.connection.js";
 import { createDefaultDatabase } from "./helpers/common.helpers..js";
+import cluster from "cluster";
+import os from "os";
 dotenv.config();
 const fileName = fileURLToPath(import.meta.url);
 const dirName = dirname(fileName);
@@ -31,14 +33,52 @@ const logger = PinoLogger.pino({
     },
 }, pinoms.multistream(streams));
 global.logger = logger;
-initializeDatabase()
-    .then(() => {
-    logger.info(`Database connection ✔`);
-    initializeServer()
+// initializeDatabase()
+//   .then(() => {
+//     logger.info(`Database connection ✔`);
+//     initializeServer()
+//       .then(() => {
+//         createDefaultDatabase()
+//           .then(() => {
+//             logger.info(`Basic Data Created ✔`);
+//           })
+//           .catch((e) => {
+//             logger.error(e);
+//           });
+//       })
+//       .catch((e) => {
+//         logger.error(e);
+//       });
+//   })
+//   .catch((e) => {
+//     logger.error(e);
+//   });
+const numCPUs = os.cpus().length;
+if (cluster.isPrimary) {
+    console.log(`Primary ${process.pid} is running`);
+    // Fork workers for each CPU core
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+    // Handle worker exit
+    cluster.on("exit", (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died, starting a new one`);
+        cluster.fork(); // Restart a new worker if one dies
+    });
+}
+else {
+    initializeDatabase()
         .then(() => {
-        createDefaultDatabase()
+        logger.info(`Database connection ✔`);
+        initializeServer()
             .then(() => {
-            logger.info(`Basic Data Created ✔`);
+            createDefaultDatabase()
+                .then(() => {
+                logger.info(`Basic Data Created ✔`);
+            })
+                .catch((e) => {
+                logger.error(e);
+            });
         })
             .catch((e) => {
             logger.error(e);
@@ -47,10 +87,7 @@ initializeDatabase()
         .catch((e) => {
         logger.error(e);
     });
-})
-    .catch((e) => {
-    logger.error(e);
-});
+}
 process
     .on("unhandledRejection", (response, p) => {
     console.log(response);
@@ -59,23 +96,4 @@ process
     .on("uncaughtException", (err) => {
     logger.error(err);
 });
-// const cluster = require('node:cluster');
-// const numCPUs = require('node:os').availableParallelism();
-// const process = require('node:process');
-// if (cluster.isPrimary) {
-//   console.log(Primary ${process.pid} is running);
-//   // Fork workers for each CPU core
-//   for (let i = 0; i < numCPUs; i++) {
-//     cluster.fork();
-//   }
-//   // Handle worker exit
-//   cluster.on('exit', (worker, code, signal) => {
-//     console.log(Worker ${worker.process.pid} died, starting a new one);
-//     cluster.fork(); // Restart a new worker if one dies
-//   });
-// } else {
-//   // Each worker runs the Express app
-//   require('./app'); // Import the express app from a separate file
-// }
-// { $sort: { [options.sortBy]: options.sortOrder, _id: 1 } },
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ import { initializeServer } from "./connections/http.connection.js";
 import dotenv from "dotenv";
 import { initializeDatabase } from "./connections/database.connection.js";
 import { createDefaultDatabase } from "./helpers/common.helpers..js";
-import { rootDir } from "./config/master.config.js";
+import cluster from "cluster";
+import os from "os";
 
 dotenv.config();
 
@@ -41,26 +42,63 @@ const logger = PinoLogger.pino(
 
 global.logger = logger;
 
-initializeDatabase()
-  .then(() => {
-    logger.info(`Database connection ✔`);
-    initializeServer()
-      .then(() => {
-        createDefaultDatabase()
-          .then(() => {
-            logger.info(`Basic Data Created ✔`);
-          })
-          .catch((e) => {
-            logger.error(e);
-          });
-      })
-      .catch((e) => {
-        logger.error(e);
-      });
-  })
-  .catch((e) => {
-    logger.error(e);
+// initializeDatabase()
+//   .then(() => {
+//     logger.info(`Database connection ✔`);
+//     initializeServer()
+//       .then(() => {
+//         createDefaultDatabase()
+//           .then(() => {
+//             logger.info(`Basic Data Created ✔`);
+//           })
+//           .catch((e) => {
+//             logger.error(e);
+//           });
+//       })
+//       .catch((e) => {
+//         logger.error(e);
+//       });
+//   })
+//   .catch((e) => {
+//     logger.error(e);
+//   });
+
+const numCPUs = os.cpus().length;
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+
+  // Fork workers for each CPU core
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  // Handle worker exit
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died, starting a new one`);
+    cluster.fork(); // Restart a new worker if one dies
   });
+} else {
+  initializeDatabase()
+    .then(() => {
+      logger.info(`Database connection ✔`);
+      initializeServer()
+        .then(() => {
+          createDefaultDatabase()
+            .then(() => {
+              logger.info(`Basic Data Created ✔`);
+            })
+            .catch((e) => {
+              logger.error(e);
+            });
+        })
+        .catch((e) => {
+          logger.error(e);
+        });
+    })
+    .catch((e) => {
+      logger.error(e);
+    });
+}
 
 process
   .on("unhandledRejection", (response, p) => {
@@ -70,28 +108,3 @@ process
   .on("uncaughtException", (err) => {
     logger.error(err);
   });
-
-// const cluster = require('node:cluster');
-// const numCPUs = require('node:os').availableParallelism();
-// const process = require('node:process');
-
-// if (cluster.isPrimary) {
-//   console.log(Primary ${process.pid} is running);
-
-//   // Fork workers for each CPU core
-//   for (let i = 0; i < numCPUs; i++) {
-//     cluster.fork();
-//   }
-
-//   // Handle worker exit
-//   cluster.on('exit', (worker, code, signal) => {
-//     console.log(Worker ${worker.process.pid} died, starting a new one);
-//     cluster.fork(); // Restart a new worker if one dies
-//   });
-
-// } else {
-//   // Each worker runs the Express app
-//   require('./app'); // Import the express app from a separate file
-// }
-
-// { $sort: { [options.sortBy]: options.sortOrder, _id: 1 } },
